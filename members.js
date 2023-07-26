29d9b6e732469829b32ae6e40516c6ff08923e4c
pipadev <42921255+pipadev@users.noreply.github.com>
2023-07-26 13:33:32 +0000

    Another copilot example

---
+++ members.js
@@ -0,0 +1,20 @@
+function skillsMembers() {
+    var members = [
+        {
+            name: "John Doe",
+            skills: ["JavaScript", "Python", "HTML", "CSS"]
+        },
+        {
+            name: "Jane Doe",
+            skills: ["JavaScript", "NodeJS", "ReactJS", "HTML", "CSS"]
+        },
+        {
+            name: "Jim Doe",
+            skills: ["JavaScript", "Python", "HTML", "CSS"]
+        },
+        {
+            name: "Jill Doe",
+            skills: ["JavaScript", "NodeJS", "ReactJS", "HTML", "CSS"]
+        }
+    ];
+}
